bfbb63d3aadad476614c2c5fc8824724ad42eb7b
Shashwat Mishra <shashwatmishra466@gmail.com>
2024-11-07 23:20:03 +0530

    learn : stream pipes

--- streamAndBuffer/streamAndBuffer.js
+++ streamAndBuffer/streamAndBuffer.js
@@ -6,19 +6,21 @@ const server = http.createServer();
 server.on("request",(req, res) => {
 const rstream = fs.createReadStream("input.txt")
 
-rstream.on("data",(chunkData) => {
-    res.write(chunkData);
-});
+// rstream.on("data",(chunkData) => {
+//     res.write(chunkData);
+// });
 
-rstream.on("end", () => {
-    res.end();
-})
+// rstream.on("end", () => {
+//     res.end();
+// })
 
-rstream.on("error",(err) => {
-    console.log(err);
+// rstream.on("error",(err) => {
+//     console.log(err);
 
-    res.end("file not found")
-})
+//     res.end("file not found")
+// })
+
+rstream.pipe(res);
 })
 
 

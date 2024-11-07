faed700d8e9fb1e2b8d8e80c4f2c6413014d773e
Shashwat Mishra <shashwatmishra466@gmail.com>
2024-11-07 18:39:12 +0530

    learn: create API

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -1,5 +1,9 @@
 const http = require("http");
+const fs = require("fs")
 const server = http.createServer((req,res) => {
+    const data = fs.readFileSync(`${__dirname}/../jsonNode/userApi/userData.json`,"utf-8")
+    const objData = JSON.parse(data);
+        
     console.log(req.url);
     if(req.url == "/"){
     res.end("hello guys i am shashwat mishra ji")
@@ -7,6 +11,10 @@ const server = http.createServer((req,res) => {
         res.end("this about page")
     }else if(req.url == "/contact"){
         res.end("this is contact page")
+    }else if(req.url == "/userapi"){
+        // console.log(objData)
+        res.writeHead(200, {"content-type": "application/json"});
+        res.end(objData[1].age.toString());
     }else{
         res.writeHead(404,{"content-type":"text/html"})
 

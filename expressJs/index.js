6565b9f00a8da5b34bb71f2da46494616a9e3136
Shashwat Mishra <shashwatmishra466@gmail.com>
2024-11-13 17:09:37 +0530

    learn: middleware

---
+++ expressJs/index.js
@@ -0,0 +1,70 @@
+const express = require("express")
+const fs = require("fs");
+const { type } = require("os");
+const server = express();
+
+const data = JSON.parse(fs.readFileSync(`${__dirname}/../jsonNode/userApi/userData.json`, "utf-8"));
+// console.log(data)
+
+
+
+const auth = (req, res, next) => {
+    console.log(req.query)
+    if(req.query.password=='123'){
+        next();
+    }else{
+        res.sendStatus(401);
+    }
+    
+}
+//logger - middleware
+
+server.use((req, res, next) => {
+    console.log(req.method, req.ip, new Date(), req.hostname, req.get('User-Agent'))
+    next();
+})
+//logger - middleware
+
+//API - Endpoint - Route
+server.get('/', auth ,(req,res) => {
+    res.json({type:'GET'})
+})
+
+server.post('/', auth ,(req,res) => {
+    res.json({type:'POST'})
+})
+
+server.put('/',auth ,(req, res) => {
+    res.json({type:'PUT'})
+})
+
+server.delete('/',auth, (req, res) => {
+    res.json({type:'DELETE'})
+})
+
+server.patch('/', auth ,(req, res) => {
+    res.json({type:'PATCH'})
+})
+
+
+
+
+server.get('/demo', (req, res) => {
+    res.send(JSON.stringify(data))
+    // res.json(data)
+
+    // res.sendFile("absolute directory")
+})
+
+
+
+
+
+
+
+
+
+
+server.listen(8000, () => {
+    console.log('server started........')
+})

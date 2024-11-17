ab3cdda575d422e6232a62cb190ff114aed7788d
Shashwat Mishra <shashwatmishra466@gmail.com>
2024-11-17 09:30:40 +0530

    practice

--- REST_API/index.js
+++ REST_API/index.js
@@ -1,3 +1,24 @@
+// const fs = require("fs")
+// PORT = "8000"
+// const express = require("express")
+// const server = express();
+// //bodyParser
+// server.use(express.json())
+// const dataController = require("./controller/data");
+// const { router } = require("./routes/product");
+// // const { router } = require("./routes/product");
+// // const { router } = require("./routes/product");
+
+// server.use("/data",router);
+// server.listen(PORT , () => {
+//     console.log("server started.......")
+//     console.log("server is running")
+
+// })
+
+
+
+
 const fs = require("fs")
 PORT = "8000"
 const express = require("express")
